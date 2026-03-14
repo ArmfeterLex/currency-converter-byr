d373dbe29e5f97dbe6b8235c689414011cfdbb12
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-15 00:36:10 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите стоимость товара в белорусских рублях");
+        int a = int.Parse(Console.ReadLine());
+        double b = Math.Round((a * 28.8),2); double c = Math.Round((a * 0.2737), 2); double d = Math.Round((a*12.6), 2);
+        Console.WriteLine("Стоимость товара в русских рублях {0:f2}; в евро {1:f2}; в гривнах {2:f2}", b, c, d);
+        Console.ReadKey();
+    }
+}
